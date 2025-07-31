97d3ef90781154a024ef14df27d15a1f4b702fb4
hustler78237 <160506938+hustler78237@users.noreply.github.com>
2025-07-31 11:46:05 +0530

    Add files via upload

--- Show.js
+++ Show.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function Show() {
+
+    // const[loca, setLoca] = useState('');
+    // function handleloaction(value) {
+    //     setLoca(input = value);
+    // }
+
     return(
-        <div className="bg-gray-700 bg-cover h-screen">
-            <div></div>
+        <div className="bg-gray-700 bg-cover h-screen flex items-center justify-center ">
+            <div className="bg-black w-96 ">
+               <div className="flex justify-center">
+               <input type="serach" className="mt-2" />
+               </div>
+               <div className="text-white flex justify-center">
+                Show location
+               </div>
+               <div className="text-white">
+                16C
+               </div>
+               <div className="text-white">
+                 image of status
+               </div>
+               <div className="text-white">
+                wind speed and many more
+               </div>
+               <div className="text-white">
+               conculsion of weather here
+               </div>
+            </div>
         </div>
     )
 }
